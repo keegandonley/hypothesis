2a9b713552a1de8e7da5bdf6fcecdcb32c56ad10
Keegan Donley <kd@keegandonley.com>
2026-03-09 13:14:16 -0500

    analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { useBranding, brandingToCssVars } from '../lib/branding';
+import { Analytics } from '@vercel/analytics/react';
 
 function App({ Component, pageProps }: AppProps) {
   const branding = useBranding();
   return (
     <div style={brandingToCssVars(branding.colors)}>
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
