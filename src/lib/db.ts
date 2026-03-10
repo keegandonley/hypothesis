45c8e1b780af0139a460982a96bba9c2b284c5a9
Keegan Donley <kd@keegandonley.com>
2026-03-10 12:20:21 -0500

    set rejectunauthorized

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,5 +6,5 @@ export const pool = new Pool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: true },
 });
